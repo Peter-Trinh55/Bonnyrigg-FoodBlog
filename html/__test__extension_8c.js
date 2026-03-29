1ad7ebf9ec5bdd78d8a025b89e353cb52b6d4ef3
HenryPham3023 <henry.pham14@education.nsw.gov.au>
2026-03-30 03:15:23 +1100

    Files may include personal data such as usernames, emails, or local system paths.
    Removing such files protects user privacy and ensures compliance with data protection practices, especially when the repository is public.

--- html/__test__extension_8c.js
+++
@@ -1,7 +0,0 @@
-var __test__extension_8c =
-[
-    [ "INITERROR", "__test__extension_8c.html#a3d94077229c2876134769daeeb28fa8a", null ],
-    [ "Py_RETURN_NONE", "__test__extension_8c.html#abe8e5f15421fd71664dd8332e5ad5b63", null ],
-    [ "TEST_MODULE_NAME", "__test__extension_8c.html#a4264a8d446450182d2f0803c9d0f47b0", null ],
-    [ "PyInit__test_extension", "__test__extension_8c.html#acdd4dad96fc38b4a10ba97b9a59cca2b", null ]
-];
